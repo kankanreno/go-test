6520b42a4785357b2b4deb9dd54418215b9731c0
kankanreno <kankan.123>
2019-07-15 03:16:31 +0800

    test-pipeline generate file

---
+++ test-pipeline/main.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"test-go/test-pipeline/pipeline"
+)
+
+func merge() {
+	p := pipeline.Merge(
+		pipeline.InMemSort(pipeline.ArraySource(3, 2, 6, 7, 4)),
+		pipeline.InMemSort(pipeline.ArraySource(7, 1, 3, 9, 5)))
+
+	//for {
+	//	if num, ok := <-p; ok {
+	//		fmt.Println(num)
+	//	} else {
+	//		break
+	//	}
+	//}
+
+	for v := range p {
+		fmt.Println(v)
+	}
+}
+
+func main() {
+	const filename = "large.log"
+	const n = 100000000
+
+	file, err := os.Create(filename)
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+
+	p := pipeline.RandomSource(n)
+	writer := bufio.NewWriter(file)
+	pipeline.WriterSink(writer, p)
+	writer.Flush()
+
+	file, err = os.Open(filename)
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+
+	p = pipeline.ReaderSource(bufio.NewReader(file))
+	count := 0
+	for v := range p {
+		fmt.Println(v)
+		count++
+		if count >= 100 {
+			break
+		}
+	}
+}
