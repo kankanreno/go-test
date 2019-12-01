f43a5c64e5d150dff764d0f28d0f601f3c99f4d3
kankanreno <kankan.123>
2019-12-01 15:16:11 +0800

    slice with go-funk

--- slice/main.go
+++ slice/main.go
@@ -2,12 +2,17 @@ package main
 
 import (
 	"fmt"
-    "github.com/thoas/go-funk"
+	"github.com/spf13/cast"
+	"github.com/thoas/go-funk"
 )
 
 func main() {
-	r := funk.Filter([]int{1, 2, 3, 4}, func(x int) bool {
-		return x != 2
-	}) // []int{2, 4}
-    fmt.Println(r)
+	var id uint = 2
+	r := funk.Filter([]string{"1", "2", "3", "4"}, func(x string) bool {
+		return x != cast.ToString(id)
+	})
+	fmt.Println(r)
+	for _, v := range r.([]string) {
+		fmt.Println(v)
+	}
 }
