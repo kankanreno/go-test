d9c57aa9cd6955b929afb8539f89646fe3fe4e2b
kankan <kankanreno@163.com>
2019-12-17 10:58:16 +0800

    goroutine

--- goroutine/main.go
+++ goroutine/main.go
@@ -1,70 +1,27 @@
 package main
 
 import (
-	"fmt"
-	"github.com/sirupsen/logrus"
+	"log"
 	"sync"
-	"time"
 )
 
-func printNumber(from, to int, c chan int) {
-	for x := from; x <= to; x++ {
-		fmt.Printf("%d\n", x)
-		time.Sleep( 1 * time.Millisecond)
-	}
-
-	c <- 0
-}
-
-//func main() {
-//	//c := make(chan int, 3)
-//	//go printNumber(1, 3, c)
-//	//go printNumber(4, 6, c)
-//	//_ = <- c
-//	//_ = <- c
-//	go func() {
-//		fmt.Println("go1 begin...")
-//		go func() {
-//			fmt.Println("go2 begin...")
-//			time.Sleep(1 * time.Second)
-//			fmt.Println("go2 end")
-//		}()
-//		fmt.Println("go1 end")
-//	}()
-//
-//	time.Sleep(3 * time.Second)
-//}
-
 var wg sync.WaitGroup
-var mux sync.Mutex
 
-type Data struct {
-	ID    int
-}
-
-func foo(i int, data *Data) {
+func add(i int) {
 	defer wg.Done()
 
-	logrus.Infof("data address: %p", data)
-	mux.Lock()
-	data.ID = i
-	if data.ID != i {
-		logrus.Info(i, data.ID)
+	x := 0
+	for i := 0; i < 10000000000; i++ {
+		x++
 	}
-	mux.Unlock()
+	log.Printf("%d, %d", i, x)
 }
 
 func main() {
-	//data1 := &Data{}
-	//logrus.Infof("data1 address: %p", data1)
-	//data2 := &Data{}
-	//logrus.Infof("data2 address: %p", data2)
-
-	data := &Data{}
-	for i := 0; i < 100; i++ {
+	for i := 0; i < 10; i++ {
 		wg.Add(1)
 
-		go foo(i, data)
+		go add(i)
 	}
 
 	wg.Wait()
